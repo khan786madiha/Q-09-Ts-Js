abccee57d75e66319d17be9c7e15c4fe28e9430c
Madiha Khan <159880897+khan786madiha@users.noreply.github.com>
2024-02-23 03:49:07 -0800

    Q-09 Ts/Js & node.js of 45 assignments
    Madiha Khan's NODEJS 45 Assignment Q-09

---
+++ Q9.ts
@@ -0,0 +1,11 @@
+// 9.	Favorite Number: Store your favorite number in a variable. Then, using that variable,
+//  create a message that reveals your favorite number. Print that message.
+
+let fNum: number = 78364
+
+console.log(`My Favourite Number Is ${fNum}`);
+
+// 2ND EXAMPLE
+let favoriteNumber: number = 7; // Example favorite number
+
+console.log("My favorite number is: " + favoriteNumber);
